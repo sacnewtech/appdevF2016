d89546477ac1ca13d9441d41a73001084626eaf9
Jerry Huang <jerry.huang@snths.org>
2016-08-23 13:58:24 -0700

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,10 +1,8 @@
 package com.weebly.jeribeartx.sophomoretrip;
 
 import android.app.Activity;
-import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.TypedValue;
@@ -14,8 +12,6 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ListAdapter;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.graphics.Color;
@@ -28,16 +24,19 @@ public class MainActivity extends Activity {
     public final static String EXTRA_COMMENTS = "com.weebly.jeribeartx.sophomoretrip.COMMENTS";
     public final static String EXTRA_CITY1 = "com.weebly.jeribeartx.sophomoretrip.CITY1";
     public final static String EXTRA_CITY2 = "com.weebly.jeribeartx.sophomoretrip.CITY2";
+    public final static String EXTRA_DURATION = "com.weebly.jeribeartx.sophomoretrip.DURATION";
 
-    private String cities[] = {"Monterey Bay", "Los angeles",
+
+
+    public String item1;
+    public String item2;
+    public String item3;
+    public EditText nameField;
+    public EditText commentsField;
+
+    private String cities[] = {"Monterey Bay", "Los Angeles",
             "Sacramento", "San Diego", "San Francisco"};
     private String durations[] = {"1 week", "3 days", "1 day"};
-
-    private EditText nameField = new EditText(this);
-    private ListView cityList = new ListView(this);
-	private ListView cityList2 = new ListView(this);
-	private EditText commentsField = new EditText(this);
-
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent event) {
@@ -66,14 +65,41 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        ListView cityList1;
+        ListView cityList2;
+        item1 = "";
+        item2 = "";
+        item3 = "";
         TextView cityTitle = new TextView(this);
+        TextView cityTitle2 = new TextView(this);
+
+        cityTitle.setId(1);
+
+        cityList1 = new ListView(this);
+        cityList1.setId(2);
+        cityList1.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice, cities));
+        cityList1.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+        cityTitle2.setId(3);
+        cityList2 = new ListView(this);
+        cityList2.setId(4);
+        cityList2.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice, cities));
+        cityList2.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+        nameField = new EditText(this);
+        commentsField = new EditText(this);
+        nameField.setId(7);
+        commentsField.setId(8);
+
+
         cityTitle.setText("Choose Your FIRST Choice City:");
 
-        TextView cityTitle2 = new TextView(this);
+
         cityTitle2.setText("Choose Your SECOND Choice City:");
 
         TextView durationTitle = new TextView(this);
         durationTitle.setText("Choose How Long You Would Want to Stay:");
+        durationTitle.setId(5);
 
 
         nameField.setHint("First and Last Name");
@@ -81,65 +107,61 @@ public class MainActivity extends Activity {
 
 
         //nameField.setMinWidth(600);
-        nameField.setWidth ( (int) TypedValue.applyDimension (
+        nameField.setWidth((int) TypedValue.applyDimension(
                 TypedValue.COMPLEX_UNIT_DIP,
                 600,
                 getResources().getDisplayMetrics()
                 )
         );
         //commentsField.setMinWidth(800);
-        nameField.setWidth ( (int) TypedValue.applyDimension (
+        nameField.setWidth((int) TypedValue.applyDimension(
                 TypedValue.COMPLEX_UNIT_DIP,
                 800,
                 getResources().getDisplayMetrics()
                 )
         );
 
-        //ListAdapter adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_single_choice, cities);
-
-        //setListAdapter(adapter);
 
 
-        cityList.setAdapter(new ArrayAdapter<String> (this, android.R.layout.simple_list_item_single_choice, cities));
-        cityList.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 
-        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        cityList1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String item = (String) parent.getItemAtPosition(position);
-                Toast.makeText(MainActivity.this, "#1 Choice Is " + item, Toast.LENGTH_SHORT).show();
+                item1 = (String) parent.getItemAtPosition(position);
+                Toast.makeText(MainActivity.this, "#1 Choice Is " + item1, Toast.LENGTH_SHORT).show();
 
 
             }
         });
 
 
-        cityList2.setAdapter(new ArrayAdapter<String> (this, android.R.layout.simple_list_item_single_choice, cities));
-        cityList2.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
 
         cityList2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String item = (String) parent.getItemAtPosition(position);
-                Toast.makeText(MainActivity.this, "#1 Choice Is " + item, Toast.LENGTH_SHORT).show();
+                item2 = (String) parent.getItemAtPosition(position);
+                Toast.makeText(MainActivity.this, "#2 Choice Is " + item2, Toast.LENGTH_SHORT).show();
 
 
             }
         });
 
 
-        ListView duraList = new ListView(this);
-        duraList.setAdapter(new ArrayAdapter<String> (this, android.R.layout.simple_list_item_single_choice, durations));
+        ListView duraList = new ListView(getApplicationContext());
+        duraList.setId(6);
+
+        duraList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice, durations));
         duraList.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 
         duraList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String item = (String) parent.getItemAtPosition(position);
-                Toast.makeText(MainActivity.this, "Duration Choice is " + item, Toast.LENGTH_SHORT).show();
+                item3 = (String) parent.getItemAtPosition(position);
+                Toast.makeText(MainActivity.this, "Duration Choice is " + item3, Toast.LENGTH_SHORT).show();
 
 
             }
@@ -192,11 +214,18 @@ public class MainActivity extends Activity {
         submitButton.setBackgroundColor(Color.DKGRAY);
         submitButton.setTextColor(Color.WHITE);
         submitButton.setMinWidth(700);
-        submitButton.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v)
-            {
-                summaryMessage(v);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SummaryMessage.class);
+                String studentName = nameField.getText().toString();
+                String studentComments = commentsField.getText().toString();
+                intent.putExtra(EXTRA_NAME, studentName);
+                intent.putExtra(EXTRA_COMMENTS, studentComments);
+                intent.putExtra(EXTRA_CITY1, item1);
+                intent.putExtra(EXTRA_CITY2, item2);
+                intent.putExtra(EXTRA_DURATION, item3);
+                MainActivity.this.startActivity(intent);
             }
         });
 
@@ -209,19 +238,11 @@ public class MainActivity extends Activity {
 
         //Layout
         RelativeLayout tripLayout = new RelativeLayout(this);
-        cityTitle.setId(1);
-        cityList.setId(2);
-        cityTitle2.setId(3);
-        cityList2.setId(4);
 
-        durationTitle.setId(5);
-        duraList.setId(6);
-        nameField.setId(7);
-        commentsField.setId(8);
 
         cityTitleParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
         listParams.addRule(RelativeLayout.BELOW, cityTitle.getId());
-        cityTitleParams2.addRule(RelativeLayout.BELOW, cityList.getId());
+        cityTitleParams2.addRule(RelativeLayout.BELOW, cityList1.getId());
         listParams2.addRule(RelativeLayout.BELOW, cityTitle2.getId());
 
         durationTitleParams.addRule(RelativeLayout.BELOW, cityList2.getId());
@@ -230,10 +251,11 @@ public class MainActivity extends Activity {
         commentsParams.addRule(RelativeLayout.BELOW, nameField.getId());
         buttonParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
         buttonParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        buttonParams.setMargins(0,0,0,50);
 
 
         tripLayout.addView(cityTitle, cityTitleParams);
-        tripLayout.addView(cityList, listParams);
+        tripLayout.addView(cityList1, listParams);
         tripLayout.addView(cityTitle2, cityTitleParams2);
         tripLayout.addView(cityList2, listParams2);
         tripLayout.addView(durationTitle, durationTitleParams);
@@ -246,18 +268,5 @@ public class MainActivity extends Activity {
         tripLayout.addView(submitButton, buttonParams);
 
         setContentView(tripLayout);
-    }
-
-    public void summaryMessage(View view) {
-        Intent intent = new Intent(this, SummaryMessage.class);
-        String studentName = nameField.getText().toString();
-        String studentComments = commentsField.getText().toString();
-        String studentCity1 = cityList.getSelectedItem().toString();
-        String studentCity2 = cityList2.getSelectedItem().toString();
-        intent.putExtra(EXTRA_NAME, studentName);
-        intent.putExtra(EXTRA_COMMENTS, studentComments);
-        intent.putExtra(EXTRA_CITY1, studentCity1);
-        intent.putExtra(EXTRA_CITY2, studentCity2);
-        startActivity(intent);
     }
 }
